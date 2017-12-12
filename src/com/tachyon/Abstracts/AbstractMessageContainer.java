471314a388e60a77fab7d0c4ac9c532ef6f25023
Tachyonn <31632075+Tachyonn@users.noreply.github.com>
2017-12-12 17:41:59 +0200

    Implement toJSON method in AbstractMessageContainer

--- src/com/tachyon/Abstracts/AbstractMessageContainer.java
+++ src/com/tachyon/Abstracts/AbstractMessageContainer.java
@@ -1,9 +1,18 @@
 package com.tachyon.Abstracts;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 /*
 Container for messages, messageLists and server commands
 JSON from, to
 Uses Gson https://mvnrepository.com/artifact/com.google.code.gson/gson
 */
 public abstract class AbstractMessageContainer {
+
+    public String toJSON() {
+        Gson gson = new GsonBuilder().create();
+        String result = gson.toJson(this);
+        return result;
+    }
 }
