8c7daee6c6ec39225b353300f44271ab84534efa
Tachyonn <31632075+Tachyonn@users.noreply.github.com>
2017-12-12 17:29:26 +0200

    Implement TimeStamp

--- src/com/tachyon/Abstracts/AbstractTimeStamp.java
+++ src/com/tachyon/Abstracts/AbstractTimeStamp.java
@@ -1,6 +1,17 @@
 package com.tachyon.Abstracts;
 
-/* returns new Date as string */
-public class AbstractTimeStamp {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+/* returns new Date as string */
+public abstract class AbstractTimeStamp {
+    public static String get(Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        String result = sdf.format(date);
+        return result;
+    }
+
+    public static String get() {
+        return get(new Date());
+    }
 }
